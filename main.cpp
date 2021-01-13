5ae4976943750c2e63c565b43e6bc8beec3f7fb1
Trung Pham <pham.bao.trung@gmail.com>
2021-01-13 18:19:57 -0500

    Use CTS

--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
-#include <cassert>
-#include <cstring>
-#include <cmath>
-
-#include <fstream>
-#include <iostream>
-
+#include <cryptopp/files.h>
+#include <cryptopp/hkdf.h>
 #include <cryptopp/modes.h>
-#include <cryptopp/pwdbased.h>
+#include <cryptopp/osrng.h>
 #include <cryptopp/sha3.h>
 #include <cryptopp/threefish.h>
-#include <cryptopp/osrng.h>
+using namespace CryptoPP;
 
 #define PNG_DEBUG 3
 #include <png.h>
-
-#include "lz4.h"
 
 static void error_exit(std::string msg) {
     std::cerr << msg << std::endl;
@@ -57,7 +50,8 @@ unsigned char *read_png(char *file_name, uint32_t *width, uint32_t *height) {
     *height = png_get_image_height(png_ptr, info_ptr);
     int color_type = png_get_color_type(png_ptr, info_ptr);
     int bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-    if (bit_depth != 8 || color_type != PNG_COLOR_TYPE_GRAY || *width != *height) {
+    if (bit_depth != 8 || color_type != PNG_COLOR_TYPE_GRAY ||
+        *width != *height) {
         error_exit("[read_png] File does not have needed format");
     }
 
@@ -91,7 +85,8 @@ unsigned char *read_png(char *file_name, uint32_t *width, uint32_t *height) {
     return data;
 }
 
-void write_png(char *file_name, unsigned char *data, uint32_t width, uint32_t height) {
+void write_png(char *file_name, unsigned char *data, uint32_t width,
+               uint32_t height) {
 
     FILE *fp = fopen(file_name, "wb");
     if (fp == NULL) {
@@ -153,184 +148,195 @@ void write_png(char *file_name, unsigned char *data, uint32_t width, uint32_t he
 }
 
 static inline void u64_to_u8(unsigned char out[8U], uint64_t x) {
-    out[0] = (unsigned char) (x & 0xff); x >>= 8;
-    out[1] = (unsigned char) (x & 0xff); x >>= 8;
-    out[2] = (unsigned char) (x & 0xff); x >>= 8;
-    out[3] = (unsigned char) (x & 0xff); x >>= 8;
-    out[4] = (unsigned char) (x & 0xff); x >>= 8;
-    out[5] = (unsigned char) (x & 0xff); x >>= 8;
-    out[6] = (unsigned char) (x & 0xff); x >>= 8;
-    out[7] = (unsigned char) (x & 0xff);
+    out[0] = (unsigned char)(x & 0xff);
+    x >>= 8;
+    out[1] = (unsigned char)(x & 0xff);
+    x >>= 8;
+    out[2] = (unsigned char)(x & 0xff);
+    x >>= 8;
+    out[3] = (unsigned char)(x & 0xff);
+    x >>= 8;
+    out[4] = (unsigned char)(x & 0xff);
+    x >>= 8;
+    out[5] = (unsigned char)(x & 0xff);
+    x >>= 8;
+    out[6] = (unsigned char)(x & 0xff);
+    x >>= 8;
+    out[7] = (unsigned char)(x & 0xff);
 }
 
 static inline uint64_t u8_to_u64(const unsigned char in[8U]) {
     uint64_t x;
 
-    x  = in[7]; x <<= 8;
-    x |= in[6]; x <<= 8;
-    x |= in[5]; x <<= 8;
-    x |= in[4]; x <<= 8;
-    x |= in[3]; x <<= 8;
-    x |= in[2]; x <<= 8;
-    x |= in[1]; x <<= 8;
+    x = in[7];
+    x <<= 8;
+    x |= in[6];
+    x <<= 8;
+    x |= in[5];
+    x <<= 8;
+    x |= in[4];
+    x <<= 8;
+    x |= in[3];
+    x <<= 8;
+    x |= in[2];
+    x <<= 8;
+    x |= in[1];
+    x <<= 8;
     x |= in[0];
 
     return x;
 }
 
-const unsigned int KEY_LENGTH = 128;
-const unsigned int CTR_LENGTH = 128;
-const unsigned int TWEAK_LENGTH = 16;
-const unsigned int SALT_LENGTH = 64;
-const unsigned int HSALT_LENGTH = 64;
-const unsigned int HMAC_LENGTH = 64;
-const unsigned int FILE_LENGTH = 8;
-const unsigned int COMPRESSED_LENGTH = 8;
-const unsigned int ENC_KEY_LENGTH = KEY_LENGTH + CTR_LENGTH + TWEAK_LENGTH;
-const unsigned int HEADER_LENGTH = HMAC_LENGTH + SALT_LENGTH + HSALT_LENGTH;
+const unsigned int TWEAK_SIZE = 16;
+const unsigned int HASH_KEY_SIZE = 64;
+const unsigned int HASH_SIZE = 64;
+const unsigned int SALT_SIZE = 64;
+const unsigned int IV_SIZE = 128;
+const unsigned int ENC_KEY_SIZE = 128;
+const unsigned int FILE_SIZE = 8;
+const unsigned int HKDF_SIZE =
+    ENC_KEY_SIZE + IV_SIZE + TWEAK_SIZE + HASH_KEY_SIZE;
+const std::string HEADER = "t3fcrypt001";
+const unsigned int HEADER_SIZE = 11;
 
-static void hmac(unsigned char *data, size_t data_len, char *hpwd, size_t hpwd_len, unsigned char *hsalt, size_t hsalt_len) {
-    CryptoPP::byte hkey[HMAC_LENGTH * 3];
-    CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA3_512> pbkdf2;
-    pbkdf2.DeriveKey(hkey, HMAC_LENGTH * 3, 0, (CryptoPP::byte *)hpwd, hpwd_len, hsalt, hsalt_len, 42);
-
-    std::memcpy(data, &hkey[HMAC_LENGTH * 2], HMAC_LENGTH);
-    CryptoPP::HMAC<CryptoPP::SHA3_512> hmac(hkey, HMAC_LENGTH * 2);
-    hmac.Update(data, data_len);
-    CryptoPP::byte hmac_hash[HMAC_LENGTH];
-    hmac.Final(hmac_hash);
-    std::memcpy(data, hmac_hash, HMAC_LENGTH);
-}
-
-static void t3f_ctr(unsigned char *data, size_t data_len, char *pwd, size_t pwd_len, unsigned char *salt, size_t salt_len) {
-    CryptoPP::byte buf[ENC_KEY_LENGTH];
-    CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA3_512> pbkdf2;
-    pbkdf2.DeriveKey(buf, ENC_KEY_LENGTH, 0, (CryptoPP::byte *)pwd, pwd_len, salt, salt_len, 42);
-
-    CryptoPP::ConstByteArrayParameter tweak(&buf[KEY_LENGTH + CTR_LENGTH], TWEAK_LENGTH, false);
-    CryptoPP::AlgorithmParameters params = CryptoPP::MakeParameters(CryptoPP::Name::Tweak(), tweak);
-    CryptoPP::Threefish1024::Encryption t3f(buf, KEY_LENGTH);
-    t3f.SetTweak(params);
-    CryptoPP::CTR_Mode_ExternalCipher::Encryption encryptor(t3f, &buf[KEY_LENGTH]);
-    encryptor.ProcessData(data, data, data_len);
+SecByteBlock read_key(char *keyf) {
+    try {
+        SecByteBlock key(ENC_KEY_SIZE);
+        FileSource fsource(keyf, false);
+        fsource.Attach(new ArraySink(key, key.size()));
+        fsource.Pump(key.size());
+        return key;
+    } catch (const Exception &ex) {
+        std::cerr << ex.what() << std::endl;
+        exit(-2);
+    }
 }
 
 int main(int argc, char *argv[]) {
 
-    if (argc == 10 && strcmp(argv[1], "-p") == 0 && strcmp(argv[3], "-h") == 0 && strcmp(argv[5], "-i") == 0 && strcmp(argv[7], "-o") == 0 && strcmp(argv[9], "-e") == 0) {
-        size_t pwd_len = strlen(argv[2]);
-        assert(pwd_len >= 5);
-        size_t hpwd_len = strlen(argv[4]);
-        assert(hpwd_len >= 5);
+    if (argc == 8 && strcmp(argv[1], "-k") == 0 && strcmp(argv[3], "-e") == 0 &&
+        strcmp(argv[4], "-i") == 0 && strcmp(argv[6], "-o") == 0) {
+        SecByteBlock key = read_key(argv[2]);
 
         try {
-            std::ifstream ifs(argv[6], std::ios::binary | std::ios::ate);
+            std::ifstream ifs(argv[5], std::ios::binary | std::ios::ate);
             uint64_t file_len = ifs.tellg();
-            char *raw_file = (char *)malloc(file_len);
-            if (raw_file == NULL) {
-                error_exit("[main] Unable to allocate memory");
-            }
+
+            uint32_t img_side = ceil(sqrt(HEADER_SIZE + SALT_SIZE + HASH_SIZE +
+                                          FILE_SIZE + file_len));
+            std::vector<unsigned char> img(img_side * img_side);
+
             ifs.seekg(0, std::ios::beg);
-            ifs.read(raw_file, file_len);
+            ifs.read(
+                (char *)&img[HEADER_SIZE + SALT_SIZE + HASH_SIZE + FILE_SIZE],
+                file_len);
+            ifs.close();
 
-            size_t max_compressed_size = LZ4_compressBound(file_len);
-            unsigned char *compressed_buf = (unsigned char *)malloc(max_compressed_size);
-            if (compressed_buf == NULL) {
-                error_exit("[main] Unable to allocate memory");
-            }
-            uint64_t compressed_size = LZ4_compress_default(raw_file, (char *)compressed_buf, file_len, max_compressed_size);
-            if (compressed_size == 0) {
-                error_exit("[main] LZ4_compress_default");
-            }
-            free(raw_file);
-            raw_file = NULL;
+            std::memcpy(img.data(), HEADER.data(), HEADER_SIZE);
+            SecByteBlock salt(SALT_SIZE);
+            OS_GenerateRandomBlock(false, salt, SALT_SIZE);
+            std::memcpy(&img[HEADER_SIZE], salt, SALT_SIZE);
+            u64_to_u8(
+                (unsigned char *)&img[HEADER_SIZE + SALT_SIZE + HASH_SIZE],
+                file_len);
 
-            uint32_t img_side = ceil(sqrt(HEADER_LENGTH + FILE_LENGTH + COMPRESSED_LENGTH + compressed_size));
-            std::vector<char> img_body(img_side * img_side);
-            std::memcpy(&img_body[HEADER_LENGTH + FILE_LENGTH + COMPRESSED_LENGTH], compressed_buf, compressed_size);
-            free(compressed_buf);
-            compressed_buf = NULL;
+            SecByteBlock hkdf_hash(HKDF_SIZE);
+            HKDF<SHA3_512> hkdf;
+            hkdf.DeriveKey(hkdf_hash, hkdf_hash.size(), key, key.size(), salt,
+                           salt.size(), NULL, 0);
 
-            unsigned char salt[SALT_LENGTH];
-            CryptoPP::OS_GenerateRandomBlock(false, salt, SALT_LENGTH);
-            std::memcpy(&img_body[HMAC_LENGTH], salt, SALT_LENGTH);
-            unsigned char hsalt[HSALT_LENGTH];
-            CryptoPP::OS_GenerateRandomBlock(false, hsalt, HSALT_LENGTH);
-            std::memcpy(&img_body[HMAC_LENGTH + SALT_LENGTH], hsalt, HSALT_LENGTH);
-            u64_to_u8((unsigned char *)&img_body[HEADER_LENGTH], file_len);
-            u64_to_u8((unsigned char *)&img_body[HEADER_LENGTH + FILE_LENGTH], compressed_size);
+            ConstByteArrayParameter twk(&hkdf_hash[ENC_KEY_SIZE + IV_SIZE],
+                                        TWEAK_SIZE, false);
+            AlgorithmParameters params = MakeParameters(Name::Tweak(), twk);
+            Threefish1024::Encryption t3f(&hkdf_hash[0], ENC_KEY_SIZE);
+            t3f.SetTweak(params);
+            CBC_CTS_Mode_ExternalCipher::Encryption enc(
+                t3f, &hkdf_hash[ENC_KEY_SIZE]);
+            size_t s = img.size() - (HEADER_SIZE + SALT_SIZE + HASH_SIZE);
+            ArraySink as(&img[HEADER_SIZE + SALT_SIZE + HASH_SIZE], s);
+            ArraySource(
+                &img[HEADER_SIZE + SALT_SIZE + HASH_SIZE], s, true,
+                new StreamTransformationFilter(enc, new Redirector(as)));
 
-            t3f_ctr((unsigned char *)&img_body[HEADER_LENGTH], img_body.size() - HEADER_LENGTH, argv[2], pwd_len, salt, SALT_LENGTH);
-            hmac((CryptoPP::byte *)img_body.data(), img_body.size(), argv[4], hpwd_len, hsalt, HSALT_LENGTH);
+            SecByteBlock hmac_hash(HASH_SIZE);
+            HMAC<SHA3_512> hmac;
+            hmac.SetKey(&hkdf_hash[ENC_KEY_SIZE + IV_SIZE + TWEAK_SIZE],
+                        HASH_KEY_SIZE);
+            std::memset(&img[HEADER_SIZE + SALT_SIZE], 0, HASH_SIZE);
+            hmac.Update(img.data(), img.size());
+            hmac.Final(hmac_hash);
+            std::memcpy(&img[HEADER_SIZE + SALT_SIZE], hmac_hash, HASH_SIZE);
 
-            write_png(argv[8], (unsigned char *)img_body.data(), img_side, img_side);
+            write_png(argv[7], (unsigned char *)img.data(), img_side, img_side);
 
-        } catch (CryptoPP::Exception const& ex) {
-            std::cerr << "CryptoPP::Exception caught: " << ex.what() << std::endl;
-            exit(-1);
-        } catch (std::exception const& ex) {
-            std::cerr << "std::exception caught: " << ex.what() << std::endl;
-            exit(-1);
+        } catch (const Exception &ex) {
+            std::cerr << ex.what() << std::endl;
+            exit(-3);
         }
 
-    } else if (argc == 10 && strcmp(argv[1], "-p") == 0 && strcmp(argv[3], "-h") == 0 && strcmp(argv[5], "-i") == 0 && strcmp(argv[7], "-o") == 0 && strcmp(argv[9], "-d") == 0) {
-        size_t pwd_len = strlen(argv[2]);
-        assert(pwd_len >= 5);
-        size_t hpwd_len = strlen(argv[4]);
-        assert(hpwd_len >= 5);
+    } else if (argc == 8 && strcmp(argv[1], "-k") == 0 &&
+               strcmp(argv[3], "-d") == 0 && strcmp(argv[4], "-i") == 0 &&
+               strcmp(argv[6], "-o") == 0) {
+        SecByteBlock key = read_key(argv[2]);
 
         try {
             uint32_t width, height;
-            unsigned char *img_body = read_png(argv[6], &width, &height);
+            unsigned char *img = read_png(argv[5], &width, &height);
             size_t num_pixels = width * height;
 
-            unsigned char hash_from_img[HMAC_LENGTH];
-            std::memcpy(hash_from_img, img_body, HMAC_LENGTH);
-            unsigned char hsalt[HSALT_LENGTH];
-            std::memcpy(hsalt, &img_body[HMAC_LENGTH + SALT_LENGTH], HSALT_LENGTH);
+            SecByteBlock hash_from_img(HASH_SIZE);
+            std::memcpy(hash_from_img, &img[HEADER_SIZE + SALT_SIZE],
+                        HASH_SIZE);
+            SecByteBlock salt(SALT_SIZE);
+            std::memcpy(salt, &img[HEADER_SIZE], SALT_SIZE);
 
-            hmac(img_body, num_pixels, argv[4], hpwd_len, hsalt, HSALT_LENGTH);
-            if (std::memcmp(img_body, hash_from_img, HMAC_LENGTH) != 0) {
+            SecByteBlock hkdf_hash(HKDF_SIZE);
+            HKDF<SHA3_512> hkdf;
+            hkdf.DeriveKey(hkdf_hash, hkdf_hash.size(), key, key.size(), salt,
+                           salt.size(), NULL, 0);
+
+            HMAC<SHA3_512> hmac;
+            hmac.SetKey(&hkdf_hash[ENC_KEY_SIZE + IV_SIZE + TWEAK_SIZE],
+                        HASH_KEY_SIZE);
+            SecByteBlock hmac_hash(HASH_SIZE);
+            std::memset(&img[HEADER_SIZE + SALT_SIZE], 0, HASH_SIZE);
+            hmac.Update(img, num_pixels);
+            hmac.Final(hmac_hash);
+            if (hash_from_img != hmac_hash) {
                 std::cerr << "ERROR: HMAC" << std::endl;
-                std::cerr << "FILE : " << argv[6] << std::endl;
+                std::cerr << "FILE : " << argv[7] << std::endl;
                 exit(-1);
             }
 
-            unsigned char salt[SALT_LENGTH];
-            std::memcpy(salt, &img_body[HMAC_LENGTH], SALT_LENGTH);
+            ConstByteArrayParameter twk(&hkdf_hash[ENC_KEY_SIZE + IV_SIZE],
+                                        TWEAK_SIZE, false);
+            AlgorithmParameters params = MakeParameters(Name::Tweak(), twk);
+            Threefish1024::Decryption t3f(&hkdf_hash[0], ENC_KEY_SIZE);
+            t3f.SetTweak(params);
+            CBC_CTS_Mode_ExternalCipher::Decryption dec(
+                t3f, &hkdf_hash[ENC_KEY_SIZE]);
+            size_t s = num_pixels - (HEADER_SIZE + SALT_SIZE + HASH_SIZE);
+            ArraySink as(&img[HEADER_SIZE + SALT_SIZE + HASH_SIZE], s);
+            ArraySource(
+                &img[HEADER_SIZE + SALT_SIZE + HASH_SIZE], s, true,
+                new StreamTransformationFilter(dec, new Redirector(as)));
 
-            t3f_ctr(&img_body[HEADER_LENGTH], num_pixels - HEADER_LENGTH, argv[2], pwd_len, salt, SALT_LENGTH);
+            uint64_t file_len =
+                u8_to_u64(&img[HEADER_SIZE + SALT_SIZE + HASH_SIZE]);
 
-            uint64_t file_len = u8_to_u64(&img_body[HEADER_LENGTH]);
-            uint64_t compressed_size = u8_to_u64(&img_body[HEADER_LENGTH + FILE_LENGTH]);
-            unsigned char *decompressed_buf = (unsigned char *)malloc(file_len);
-            if (decompressed_buf == NULL) {
-                error_exit("[main] Unable to allocate memory");
-            }
-            uint64_t decompressed_size = LZ4_decompress_safe((char *)&img_body[HEADER_LENGTH + FILE_LENGTH + COMPRESSED_LENGTH], (char *)decompressed_buf, compressed_size, file_len);
-            if (decompressed_size <= 0 || decompressed_size != file_len) {
-                error_exit("[main] LZ4_decompress_safe");
-            }
-            free(img_body);
-            img_body = NULL;
-
-            FILE *fp = fopen(argv[8], "wb");
+            FILE *fp = fopen(argv[7], "wb");
             if (fp == NULL) {
                 error_exit("[main] fopen");
             }
-            if (fwrite(decompressed_buf, decompressed_size, 1, fp) < 1) {
+            if (fwrite(&img[HEADER_SIZE + SALT_SIZE + HASH_SIZE + FILE_SIZE],
+                       file_len, 1, fp) < 1) {
                 error_exit("[main] fwrite");
             }
             fclose(fp);
-            free(decompressed_buf);
-            decompressed_buf = NULL;
 
-        } catch (CryptoPP::Exception const& ex) {
-            std::cerr << "CryptoPP::Exception caught: " << ex.what() << std::endl;
-            exit(-1);
-        } catch (std::exception const& ex) {
-            std::cerr << "std::exception caught: " << ex.what() << std::endl;
-            exit(-1);
+        } catch (const Exception &ex) {
+            std::cerr << ex.what() << std::endl;
+            exit(-3);
         }
 
     } else {
